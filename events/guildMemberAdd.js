8695792043ee805d6d4999d5b577f6293b3d9bf9
kTweaks <EricLegend@users.noreply.github.com>
2018-03-14 00:49:52 -0700

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,6 @@
 module.exports = member => {
 
   const guild = member.guild;
-  const offtopic = guild.channels.find('name', 'offtopic');
-  return guild.channels.get(offtopic.id).send(`remember ${member.user} speak :taco:s`);
+  const botcommands = guild.channels.find('name', 'bot-commands');
+  return guild.channels.get(botcommands.id).send(`remember ${member.user} speak :taco:s`);
   };
